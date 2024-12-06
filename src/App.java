647abb0062052adaf3fcd56cc56beb82b959fa07
yousf167 <149330054+Yousf167@users.noreply.github.com>
2024-12-06 18:11:26 +0200

    Refactored Client to the new DB

--- src/App.java
+++ src/App.java
@@ -3,14 +3,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class App {
-    public static final String DB_URL = "jdbc:sqlite:src/database.db";
+    public static final String DB_URL = "jdbc:sqlite:./databaseIMS.db";
 
-    public static void main(String[] args) throws SQLException {
-        Connection conn = DriverManager.getConnection(DB_URL);
-        try {
-            System.out.println(Admin.addSupplier("sup5", "sup5@email", "1111"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws SQLException 
+    {
+        
     }
 }
