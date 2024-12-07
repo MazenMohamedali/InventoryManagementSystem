b63b847a8984b31eaaa0ad28097d9e6f87c519b3
yousf167 <149330054+Yousf167@users.noreply.github.com>
2024-12-07 10:42:00 +0200

    added Client.updateData()

--- src/App.java
+++ src/App.java
@@ -1,26 +1,44 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
-public class App {
-<<<<<<< HEAD
-    public static final String DB_URL = "jdbc:sqlite:C:\\Users\\Msi\\Desktop\\GitHub\\InventoryManagementSystem\\databaseIMS.db";
-
-    public static void main(String[] args) throws SQLException {
-
-        Supplier s = new Supplier("sup3", "sup3@email", "2222");
-        Connection conn = DriverManager.getConnection(DB_URL);
-        try {
-            System.out.println(Admin.addPhoneNumber(conn, 3, "2222"));
-        } catch (Exception e) {
-            e.printStackTrace();
+public class App 
+{
+    public static void main(String[] args) 
+    {  
+        /* 
+        for(int i = 0; i < 100; i++)
+        {
+            Client c = new Client("x" + i,"x" + i,"x" + i,"x" + i,"x" + i,0);
+            Client.addToClientTable(c);
         }
-=======
-    public static final String DB_URL = "jdbc:sqlite:./databaseIMS.db";
-
-    public static void main(String[] args) throws SQLException 
-    {
         
->>>>>>> 647abb0062052adaf3fcd56cc56beb82b959fa07
+        try
+        {
+            ArrayList <Client> c = new ArrayList<Client>(); 
+            c.addAll(Client.getData(10101));
+            for(Client cl : c)
+            {
+                System.out.println(cl.getName() + " " + cl.getBalance());
+            }
+        }
+        catch(SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        
+        try
+        {
+            ArrayList <Client> c = new ArrayList<Client>(); 
+            c.addAll(Client.getData(10101));
+            c.get(0).setName("update");
+            Client.updateData(c.get(0), 10101);
+        }
+        catch(SQLException e)
+        {
+            System.out.println(e.getMessage());
+        } 
+        */
     }
 }
